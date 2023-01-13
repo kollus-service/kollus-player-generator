9e01ad08cc48e3617038c76973b28e0b14782e92
6lueparr0t <6lueparr0t@gmail.com>
2023-01-13 15:57:23 +0900

    rename find-kollus-error -> kollus-player-generator

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 export const prefix =
   process.env.NODE_ENV === "production"
-    ? "https://kollus-service.github.io/find-player-error"
+    ? "https://kollus-service.github.io/kollus-player-generator"
     : "";
