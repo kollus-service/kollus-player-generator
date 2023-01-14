46517a1eb2b6beec9a44fdc46b6c1fa301ca3906
6lueparr0t <6lueparr0t@gmail.com>
2023-01-15 03:25:22 +0900

    Deploy Next.js to gh-pages

--- out/_next/static/MovEKh4p7h1JTd59I8p8K/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fec483df-04ddfa937c2ab123.js","static/chunks/383-a7c38e6050f5d075.js","static/chunks/pages/index-50725228a71dec1f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
